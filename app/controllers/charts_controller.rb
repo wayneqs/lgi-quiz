63bc3a35bcf1e99d18c32fbe09c9430687665bae
Wayne Quinlivan <wayneqs@gmail.com>
2019-12-01 18:27:02 +0000

    Use correct aggregate function to compute team stats

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -2,6 +2,6 @@ class ChartsController < ApplicationController
     skip_before_action :ensure_user_signed_up
 
     def team_stats
-        render json: Quiz.joins(:user).group('users.team').sum(:score)
+        render json: Quiz.joins(:user).group('users.team').average(:score)
     end
 end
