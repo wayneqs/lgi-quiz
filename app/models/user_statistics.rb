c41d47934aea7ba4b543d473e9fde29d446a1dac
Wayne Quinlivan <wayneqs@gmail.com>
2019-12-01 21:44:01 +0000

    Change query to use db native row numbering

--- app/models/user_statistics.rb
+++ app/models/user_statistics.rb
@@ -1,26 +1,28 @@
 class UserStatistics
     def initialize(this_many = 7)
-        @this_many = this_many
+        @this_many = this_many.to_i
         @leaders = []
     end
 
     attr_reader :leaders
 
     def compute
-        @results = Quiz
-                    .joins(:user)
-                    .select("users.id as user_id,
-                            users.name as username,
-                            quizzes.score as score, 
-                            (quizzes.end_time - quizzes.start_time) as time")
-                    .order(score: :desc, time: :asc)        
-        @leaders = @results.take(@this_many).map { |result| result.username }
+        @leaders = Quiz.connection.select_all("select
+                                                row_number() over(order by quizzes.score desc, 
+                                                                    (quizzes.end_time - quizzes.start_time)
+                                                            ) as position,
+                                                users.id as user_id,
+                                                users.name as username,
+                                                quizzes.score as score
+                                            from users inner join quizzes
+                                                on users.id = quizzes.user_id
+                                            order by position limit #{@this_many};").to_a      
         self
     end
 
     def find_place(user_id)
-        compute unless @results # prefer cached
-        idx = @results.map(&:user_id).index(user_id)
-        (idx + 1).ordinalize if idx
+        compute unless @leaders # prefer cached
+        leader = @leaders.detect {|leader| leader["user_id"] == user_id}
+        leader["position"].ordinalize if leader
     end
 end
