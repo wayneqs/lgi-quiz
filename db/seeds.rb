cc45b6afba8c5503b036df7b61ef9437518791d0
Wayne Quinlivan <wayneqs@gmail.com>
2019-12-09 14:10:46 +0000

    Add final question set

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,9 +4,35 @@ File.open("db/usernames.txt") { |f|
     Moniker.insert_all(moniker_data)   
 }
 
-# load the questions
-require_relative 'the_questions'
-TheQuestions.new.questions.each { |question| Question.create(question) }
-
 # load the admin user
 User.create(name: "XYZ@987", is_admin: true)
+
+def extract_option_data(line)
+    (prompt, score) = line.split("|")
+    return {
+        prompt: prompt,
+        score: score ||= 1
+    }
+end
+
+line_count = 1
+question = {}
+IO.foreach("db/questions.txt") { |line|
+    unless line.strip.empty?
+        if line_count == 1
+            question = {}
+            question[:prompt] = line
+            question[:question_options_attributes] = []
+        else
+            option = extract_option_data(line)
+            question[:question_options_attributes] << option
+        end
+
+        if line_count == 5
+            line_count = 0
+            Question.create(question)
+        end
+
+        line_count += 1
+    end
+}
